362a261246adc580c3fb6f8e539902c17d56bb0b
Norias1011 <anthonykinzelin@gmail.com>
2024-05-13 15:25:51 +0200

    new function

--- dinner.c
+++ dinner.c
@@ -6,7 +6,7 @@
 /*   By: akinzeli <akinzeli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 11:17:18 by akinzeli          #+#    #+#             */
-/*   Updated: 2024/05/13 15:04:31 by akinzeli         ###   ########.fr       */
+/*   Updated: 2024/05/13 15:25:31 by akinzeli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,15 @@ int	dinner(t_philo *philo, t_fork *fork, t_init_data *data)
 	int i;
 
 	i = 0;
+	while (i < data->number_philo)
+	{
+		(philo + i)->data = data;
+		if (pthread_create(&philo->id_thread, NULL, &philo[i]) != 0)
+		{
+			printf("Error with Pthread\n");
+			return (0);
+		}
+		i++;
+	}
+	return (1);
 }
